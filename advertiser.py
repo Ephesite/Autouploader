5932e8b1b999ee923706833f28d04a68f05dea71
IOnlySpeakMexican <93114188+IOnlySpeakMexican@users.noreply.github.com>
2021-12-07 23:21:46 -0800

    Create advertiser.py

---
+++ advertiser.py
@@ -0,0 +1,153 @@
+import requests
+import time
+def adervrtise(gameId):
+    link222 = f'https://www.roblox.com/games/{gameId}/'
+    messageE = f'''
+        reborncondos.xyz
+        :money_with_wings: Cheap admin
+        :green_circle:VERY Good Uptime 
+        {link222}
+    '''
+    jsonn = {
+        'content': f'{messageE}',
+        'tts': 'false'
+    }
+
+    headers = {
+        'authorization': "NjQ3MjgxMDE3NTgwNjgzMjg2.YZc1CQ.5SOMmSJfjotdCAfr9FOLEoHq_-w"
+    }
+    r5 = requests.post('https://discord.com/api/v9/channels/898475024242077786/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r5.status_code == 429:
+        sheesh = r5.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r5.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r5.status_code)
+    r6 = requests.post('https://discord.com/api/v9/channels/899999268146921502/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r6.status_code == 429:
+        sheesh = r6.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r6.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r6.status_code)
+    r7 = requests.post('https://discord.com/api/v9/channels/908880597915676702/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r7.status_code == 429:
+        sheesh = r7.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r7.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r7.status_code)
+    r8 = requests.post('https://discord.com/api/v9/channels/909795833334104135/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r8.status_code == 429:
+        sheesh = r8.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r8.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r8.status_code)
+    r9 = requests.post('https://discord.com/api/v9/channels/861702029561561088/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r9.status_code == 429:
+        sheesh = r9.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r9.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r9.status_code)
+    r10 = requests.post('https://discord.com/api/v9/channels/904318861770391613/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r10.status_code == 429:
+        sheesh = r10.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r10.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r10.status_code)
+    r11 = requests.post('https://discord.com/api/v9/channels/905335718577057832/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r11.status_code == 429:
+        sheesh = r11.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r11.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r10.status_code)
+    r12 = requests.post('https://discord.com/api/v9/channels/900464171244732496/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r12.status_code == 429:
+        sheesh = r12.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r12.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r12.status_code)
+    r13 = requests.post('https://discord.com/api/v9/channels/902969880922427463/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r13.status_code == 429:
+        sheesh = r13.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r13.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r13.status_code)
+    r18 = requests.post('https://discord.com/api/v9/channels/902754350848430120/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r18.status_code == 429:
+        sheesh = r18.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r18.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r18.status_code)
+    r14 = requests.post('https://discord.com/api/v9/channels/893200803022667786/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r14.status_code == 429:
+        sheesh = r14.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r14.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r14.status_code)
+    r15 = requests.post('https://discord.com/api/v9/channels/904588066415599647/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r15.status_code == 429:
+        sheesh = r15.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r15.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r15.status_code)
+    r16 = requests.post('https://discord.com/api/v9/channels/908702467821297694/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r16.status_code == 429:
+        sheesh = r16.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r16.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r16.status_code)
+    r17 = requests.post('https://discord.com/api/v9/channels/870050687457841213/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r17.status_code == 429:
+        sheesh = r17.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r17.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r17.status_code)
+    r18 = requests.post('https://discord.com/api/v9/channels/899425539293605938/messages', headers=headers, json = jsonn)
+    time.sleep(5)
+    if r18.status_code == 429:
+        sheesh = r18.json()['retry_after']
+        print(f' [Reborn] Error Slowmode {sheesh}')
+    elif r18.status_code == 200:
+        print(" [Reborn] Success sent message ")
+    else:
+        print(r18.status_code)
